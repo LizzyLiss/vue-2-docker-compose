0ecc787d9374c5a496dac96911bfb27cb4ea8591
Елизавета Дулова <liza.dulova.01@mail.ru>
2024-11-01 22:47:34 +0300

    Rework PlayerHand.vue

--- vue/src/components/game/PlayerHand.vue
+++ vue/src/components/game/PlayerHand.vue
@@ -1,46 +1,88 @@
 <template>
-  <div class="hand">
-    <div class="card" v-for="index in numberOfCards" :key="index" :style="index === 1 ? {'margin-left': '0'} : {}">
-      {{ index }}
+  <div class="hand-container">
+    <div :class="this.opponent ? 'hand-opponent': 'hand-player'">
+      <div
+          class="hand__card"
+          v-for="(card, index) in this.cards"
+          :key="index"
+          :style="index === 1 ? {'margin-left': '0'} : {}"
+      >
+        <GameCard
+            :type=card.type
+            :score=card.score
+            :key="index"
+            :face-down="opponent"
+            :enlarged="!opponent"
+            :index="index"
+            :on-drop="onCardDrop"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import GameCard from '@/components/game/GameCard.vue';
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'PlayerHand',
+  components: { GameCard },
   props: {
-    numberOfCards: {
-      type: Number,
-      required: true,
-      validator: value => value >= 0 && value <= 10
+    opponent: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    onCardDrop: {
+      type: Function,
+      default: null,
+    }
+  },
+  computed: {
+    ...mapGetters('gameEngine', [
+      'getGameEngine',
+    ]),
+    cards() {
+      return this.opponent ?
+          this.getGameEngine.opponent.cards :
+          this.getGameEngine.player.cards
     }
   }
 };
 </script>
 
-<style scoped>
+<style scoped lang="less">
 .hand {
   display: flex;
-  position: relative; 
-  width: 100%; 
-  height: 200px;
-  margin: 20px auto;
-  padding: 10px;
-  overflow: hidden;
+  position: absolute;
+  z-index: 666;
+  width: 80vw;
+  margin: auto;
+  justify-content: center;
+
+  &__card {
+    width: 150px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    font-size: 18px;
+    color: black;
+  }
+
+  &-player:extend(.hand) {
+    bottom: -20px;
+    height: 20vh;
+  }
+
+  &-opponent:extend(.hand) {
+    top: -25px;
+    height: 15vh;
+  }
 }
 
-.card {
-  width: 150px;
-  height: 200px;
-  background-color: #f0f0f0;
-  border: 1px solid #000;
-  border-radius: 5px;
-  margin-left: -50px;
+.hand-container {
   display: flex;
   justify-content: center;
-  align-items: center;
-  font-size: 18px;
-  color: black; 
 }
 </style>
